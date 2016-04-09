aa721f42612a2fca6b266ae60ee61c46aa93b47c
Joe Letizia <joe.letizia@gmail.com>
2016-04-09 09:37:11 -0400

    Some fun functions

---
+++ functions.exs
@@ -0,0 +1,41 @@
+defmodule ListManipulation do
+  def reverse([]) do
+    []
+  end
+  
+  def reverse(list) do
+    reverse(tl(list)) ++ [hd(list)]
+  end
+end
+
+defmodule Math do
+  def double(n) do
+    n * 2
+  end
+
+  def triple(n) do
+    n * 3
+  end
+
+  def quadruple(n) do
+    double(double(n))
+  end
+
+  def factorial(0), do: 1
+  def factorial(n) when n > 0 do
+    n * factorial(n-1)
+  end
+  def factorial(n) when n < 0 do
+    { :error, "Factorial of n < 0 is invalid" }
+  end
+
+  def sum(0), do: 0
+  def sum(n) do
+    n + sum(n - 1)
+  end
+
+  def gcd(x, 0), do: x
+  def gcd(x,y) do
+    gcd(y, rem(x,y))
+  end
+end
